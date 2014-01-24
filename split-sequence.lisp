efe06c5f9ce77d12a290af55a929e97af27f559a
Attila Lendvai <attila.lendvai@gmail.com>
2014-01-24 12:38:55 +0600

    Optimization, enable and accommodate for inlining.
    * inline local util functions
    * move them to the front of the file
    * make exported functions inlinable by lib users

--- split-sequence.lisp
+++ split-sequence.lisp
@@ -37,6 +37,42 @@
 
 (in-package :split-sequence)
 
+;; declaim it INLINE, and at the end of the file as NOTINLINE, so it can be inlined with local DECLARE's.
+(declaim (inline split-sequence split-sequence-if split-sequence-if-not split-from-start split-from-end))
+
+(defun split-from-start (position-fn sequence start end count remove-empty-subseqs)
+  (let ((length (length sequence)))
+    (loop
+       :for left := start :then (+ right 1)
+       :for right := (min (or (funcall position-fn sequence left) length)
+                          end)
+       :unless (and (= right left)
+                    remove-empty-subseqs) ; empty subseq we don't want
+       :if (and count (>= nr-elts count))
+       ;; We can't take any more. Return now.
+         :return (values subseqs left)
+       :else
+         :collect (subseq sequence left right) :into subseqs
+         :and :sum 1 :into nr-elts
+       :until (>= right end)
+     :finally (return (values subseqs right)))))
+
+(defun split-from-end (position-fn sequence start end count remove-empty-subseqs)
+  (loop
+     :for right := end :then left
+     :for left := (max (or (funcall position-fn sequence right) -1)
+                       (1- start))
+     :unless (and (= right (1+ left))
+                  remove-empty-subseqs) ; empty subseq we don't want
+     :if (and count (>= nr-elts count))
+     ;; We can't take any more. Return now.
+       :return (values (nreverse subseqs) right)
+     :else
+       :collect (subseq sequence (1+ left) right) into subseqs
+       :and :sum 1 :into nr-elts
+     :until (< left start)
+   :finally (return (values (nreverse subseqs) (1+ left)))))
+
 (macrolet ((check-bounds (sequence start end)
              (let ((length (gensym (string '#:length))))
                `(let ((,length (length ,sequence)))
@@ -123,37 +159,6 @@ stopped."
                             (position-if-not predicate sequence :start start :key key))
                           sequence start end count remove-empty-subseqs))))
 
-(defun split-from-end (position-fn sequence start end count remove-empty-subseqs)
-  (loop
-     :for right := end :then left
-     :for left := (max (or (funcall position-fn sequence right) -1)
-                       (1- start))
-     :unless (and (= right (1+ left))
-                  remove-empty-subseqs) ; empty subseq we don't want
-     :if (and count (>= nr-elts count))
-     ;; We can't take any more. Return now.
-       :return (values (nreverse subseqs) right)
-     :else
-       :collect (subseq sequence (1+ left) right) into subseqs
-       :and :sum 1 :into nr-elts
-     :until (< left start)
-   :finally (return (values (nreverse subseqs) (1+ left)))))
-
-(defun split-from-start (position-fn sequence start end count remove-empty-subseqs)
-  (let ((length (length sequence)))
-    (loop
-       :for left := start :then (+ right 1)
-       :for right := (min (or (funcall position-fn sequence left) length)
-                          end)
-       :unless (and (= right left)
-                    remove-empty-subseqs) ; empty subseq we don't want
-       :if (and count (>= nr-elts count))
-       ;; We can't take any more. Return now.
-         :return (values subseqs left)
-       :else
-         :collect (subseq sequence left right) :into subseqs
-         :and :sum 1 :into nr-elts
-       :until (>= right end)
-     :finally (return (values subseqs right)))))
+(declaim (notinline split-sequence split-sequence-if split-sequence-if-not))
 
 (pushnew :split-sequence *features*)
