a003c0626feb04720618232a7f190870a791a0ce
Paul F. Dietz <pdietz@grammatech.com>
2020-12-25 21:42:06 -0500

    Add extended-sequence tests (under sbcl) Make tests look at second return value as well. Add some tests for coverage.

--- tests.lisp
+++ tests.lisp
@@ -5,6 +5,46 @@
 
 (in-package :split-sequence/tests)
 
+#+sbcl
+(progn
+  (eval-when (:compile-toplevel :load-toplevel :execute)
+    (defclass eseq (sequence)
+      ((actual-seq :type list :initarg :actual-seq :initform nil
+                   :accessor actual-seq))
+      (:documentation "Extended sequence type in SBCL")))
+  (defmethod sb-sequence:length ((s eseq)) (length (actual-seq s)))
+  (defmethod sb-sequence:elt ((s eseq) index) (elt (actual-seq s) index))
+  (defmethod (setf sb-sequence:elt) (v (s eseq) index) (setf (elt (actual-seq s) index) v))
+  (defmethod sb-sequence:adjust-sequence ((s eseq) len &rest args)
+    (setf (actual-seq s)
+          (apply #'sb-sequence:adjust-sequence (actual-seq s) len args)))
+  (defmethod sb-sequence:make-sequence-like ((s eseq) len &rest args)
+    (make-instance 'eseq :actual-seq (apply #'sb-sequence:make-sequence-like
+                                            (actual-seq s) len args))))
+
+#-sbcl
+(defun equalp-seq (x y) (equalp x y))
+
+#+sbcl
+(defgeneric equalp-seq (x y)
+  (:documentation "EQUALP extended to extended sequence types")
+  (:method (x y) (equalp x y))
+  (:method ((x cons) (y cons))
+    (and (loop while (and (consp x) (consp y))
+               always (equalp-seq (pop x) (pop y)))
+         (equalp-seq x y)))
+  (:method ((x vector) (y vector))
+    (and (= (length x) (length y))
+         (every #'equalp-seq x y)))
+  (:method ((x eseq) y)
+    (equalp-seq (actual-seq x) y))
+  (:method (x (y eseq))
+    (equalp-seq x (actual-seq y))))
+
+(defmacro epmv (form1 form2)
+  `(equalp-seq (multiple-value-list ,form1)
+               (multiple-value-list ,form2)))
+
 (in-suite* :split-sequence)
 
 ;;; UNIT TESTS
@@ -23,7 +63,14 @@
          (list-input (coerce vector-input 'list))
          (list-output (mapcar (lambda (x) (coerce x 'list)) vector-output))
          (vector-name (intern (concatenate 'string (symbol-name name) ".VECTOR")))
-         (list-name (intern (concatenate 'string (symbol-name name) ".LIST"))))
+         (list-name (intern (concatenate 'string (symbol-name name) ".LIST")))
+         #+sbcl
+         (eseq-name (intern (concatenate 'string (symbol-name name) ".ESEQ")))
+         #+sbcl
+         (eseq-input-form `(make-instance 'eseq :actual-seq (copy-list ',list-input)))
+         #+sbcl
+         (eseq-output-form `(mapcar (lambda (x) (make-instance 'eseq :actual-seq (copy-list x)))
+                             ',list-output)))
     `(progn
        (test (,vector-name :compile-at :definition-time)
          (let ((,input-symbol ',vector-input)
@@ -34,146 +81,170 @@
          (let ((,input-symbol ',list-input)
                (,output-symbol ',list-output)
                (,index-symbol ,index-value))
+           ,@forms))
+       #+sbcl
+       (test (,eseq-name :compile-at :definition-time)
+         (let ((,input-symbol ,eseq-input-form)
+               (,output-symbol ,eseq-output-form)
+               (,index-symbol ,index-value))
            ,@forms)))))
 
 (define-test split-sequence.0 (:input (input "")
                                :output (output (""))
                                :index (index 0))
-  (is (equalp (split-sequence #\; input)
-              (values output index))))
+  (is (epmv (split-sequence #\; input)
+            (values output index))))
 
 (define-test split-sequence.1 (:input (input "a;;b;c")
                                :output (output ("a" "" "b" "c"))
                                :index (index 6))
-  (is (equalp (split-sequence #\; input)
-              (values output index))))
+  (is (epmv (split-sequence #\; input)
+            (values output index))))
 
 (define-test split-sequence.2 (:input (input "a;;b;c")
                                :output (output ("a" "" "b" "c"))
                                :index (index 0))
-  (is (equalp (split-sequence #\; input :from-end t)
-              (values output index))))
+  (is (epmv (split-sequence #\; input :from-end t)
+            (values output index))))
 
 (define-test split-sequence.3 (:input (input "a;;b;c")
                                :output (output ("c"))
                                :index (index 4))
-  (is (equalp (split-sequence #\; input :from-end t :count 1)
-              (values output index))))
+  (is (epmv (split-sequence #\; input :from-end t :count 1)
+            (values output index))))
 
 (define-test split-sequence.4 (:input (input "a;;b;c")
                                :output (output ("a" "b" "c"))
                                :index (index 6))
-  (is (equalp (split-sequence #\; input :remove-empty-subseqs t)
-              (values output index))))
+  (is (epmv (split-sequence #\; input :remove-empty-subseqs t)
+            (values output index))))
 
 (define-test split-sequence.5 (:input (input ";oo;bar;ba;")
                                :output (output ("oo" "bar" "b"))
                                :index (index 9))
-  (is (equalp (split-sequence #\; input :start 1 :end 9)
-              (values output index))))
+  (is (epmv (split-sequence #\; input :start 1 :end 9)
+            (values output index))))
 
 (define-test split-sequence.6 (:input (input "abracadabra")
                                :output (output ("" "br" "c" "d" "br" ""))
                                :index (index 11))
-  (is (equalp (split-sequence #\A input :key #'char-upcase)
-              (values output index))))
+  (is (epmv (split-sequence #\A input :key #'char-upcase)
+            (values output index))))
 
 (define-test split-sequence.7 (:input (input "abracadabra")
                                :output (output ("r" "c" "d"))
                                :index (index 7))
-  (is (equalp (split-sequence #\A input :key #'char-upcase :start 2 :end 7)
-              (values output index))))
+  (is (epmv (split-sequence #\A input :key #'char-upcase :start 2 :end 7)
+            (values output index))))
 
 (define-test split-sequence.8 (:input (input "abracadabra")
                                :output (output ("r" "c" "d"))
                                :index (index 2))
-  (is (equalp (split-sequence #\A input :key #'char-upcase :start 2 :end 7 :from-end t)
-              (values output index))))
+  (is (epmv (split-sequence #\A input :key #'char-upcase :start 2 :end 7 :from-end t)
+            (values output index))))
 
 (define-test split-sequence.9 (:input (input #(1 2 0))
                                :output (output (#(1 2) #()))
                                :index (index 0))
-  (is (equalp (split-sequence 0 input :from-end t)
-              (values output index))))
+  (is (epmv (split-sequence 0 input :from-end t)
+            (values output index))))
 
 (define-test split-sequence.10 (:input (input #(2 0 0 2 3 2 0 1 0 3))
                                 :output (output ())
                                 :index (index 8))
-  (is (equalp (split-sequence 0 input :start 8 :end 9 :from-end t :count 0 :remove-empty-subseqs t)
-              (values output index))))
+  (is (epmv (split-sequence 0 input :start 8 :end 9 :from-end t :count 0 :remove-empty-subseqs t)
+            (values output index))))
 
 (define-test split-sequence.11 (:input (input #(0 1 3 0 3 1 2 2 1 0))
                                 :output (output ())
                                 :index (index 0))
-  (is (equalp (split-sequence 0 input :start 0 :end 0 :remove-empty-subseqs t)
-              (values output index))))
+  (is (epmv (split-sequence 0 input :start 0 :end 0 :remove-empty-subseqs t)
+            (values output index))))
 
 (define-test split-sequence.12 (:input (input #(3 0 0 0 3 3 0 3 1 0))
                                 :output (output ())
                                 :index (index 10))
-  (is (equalp (split-sequence 0 input :start 9 :end 10 :from-end t :count 0)
-              (values output index))))
+  (is (epmv (split-sequence 0 input :start 9 :end 10 :from-end t :count 0)
+            (values output index))))
 
 (define-test split-sequence.13 (:input (input #(3 3 3 3 0 2 0 0 1 2))
                                 :output (output (#(1)))
                                 :index (index 6))
-  (is (equalp (split-sequence 0 input :start 6 :end 9 :from-end t :count 1 :remove-empty-subseqs t)
-              (values output index))))
+  (is (epmv (split-sequence 0 input :start 6 :end 9 :from-end t :count 1 :remove-empty-subseqs t)
+            (values output index))))
 
 (define-test split-sequence.14 (:input (input #(1 0))
                                 :output (output (#(1)))
                                 :index (index 0))
-  (is (equalp (split-sequence 0 input :from-end t :count 1 :remove-empty-subseqs t)
-              (values output index))))
+  (is (epmv (split-sequence 0 input :from-end t :count 1 :remove-empty-subseqs t)
+            (values output index))))
 
 (define-test split-sequence.15 (:input (input #(0 0))
                                 :output (output ())
                                 :index (index 1))
-  (is (equalp (split-sequence 0 input :start 0 :end 1 :count 0 :remove-empty-subseqs t)
-              (values output index))))
+  (is (epmv (split-sequence 0 input :start 0 :end 1 :count 0 :remove-empty-subseqs t)
+            (values output index))))
 
 (define-test split-sequence.16 (:input (input "a;;b;c")
                                 :output (output ("" ";;" ";" ""))
                                 :index (index 6))
-  (is (equalp (split-sequence #\; input :test-not #'eql)
-              (values output index))))
+  (is (epmv (split-sequence #\; input :test-not #'eql)
+            (values output index))))
 
 (define-test split-sequence.17 (:input (input "a;;b;c")
                                 :output (output ("" ";;" ";" ""))
                                 :index (index 0))
-  (is (equalp (split-sequence #\; input :from-end t :test-not #'eql)
-              (values output index))))
+  (is (epmv (split-sequence #\; input :from-end t :test-not #'eql)
+            (values output index))))
 
 (define-test split-sequence.18 (:input (input #(1 0 2 0 3 0 4))
                                 :output (output (#(1) #(2) #(3)))
                                 :index (index 6))
-  (is (equalp (split-sequence 0 input :count 3)
-              (values output index))))
+  (is (epmv (split-sequence 0 input :count 3)
+            (values output index))))
 
 (define-test split-sequence-if.1 (:input (input "abracadabra")
                                   :output (output ("" "" "r" "c" "d" "" "r" ""))
                                   :index (index 11))
-  (is (equalp (split-sequence-if (lambda (x) (member x '(#\a #\b))) input)
-              (values output index))))
+  (is (epmv (split-sequence-if (lambda (x) (member x '(#\a #\b))) input)
+            (values output index))))
 
 (define-test split-sequence-if.2 (:input (input "123456")
                                   :output (output ("1" "3" "5"))
                                   :index (index 6))
-  (is (equalp (split-sequence-if (lambda (x) (evenp (parse-integer (string x)))) input
+  (is (epmv (split-sequence-if (lambda (x) (evenp (parse-integer (string x)))) input
                                  :remove-empty-subseqs t)
-              (values output index))))
+            (values output index))))
 
 (define-test split-sequence-if.3 (:input (input "123456")
                                   :output (output ("1" "3" "5" ""))
                                   :index (index 6))
-  (is (equalp (split-sequence-if (lambda (x) (evenp (parse-integer (string x)))) input)
-              (values output index))))
+  (is (epmv (split-sequence-if (lambda (x) (evenp (parse-integer (string x)))) input)
+            (values output index))))
+
+(define-test split-sequence-if.4 (:input (input "12121212121")
+                                  :output (output ("12121" "1" "1" "1"))
+                                  :index (index 0))
+  (is (epmv (let ((count 3))
+              (split-sequence-if (lambda (c) (and (eql c #\2) (>= (decf count) 0)))
+                                 input
+                                 :from-end t))
+            (values output index))))
 
 (define-test split-sequence-if-not.1 (:input (input "abracadabra")
                                       :output (output ("ab" "a" "a" "ab" "a"))
                                       :index (index 11))
-  (is (equalp (split-sequence-if-not (lambda (x) (member x '(#\a #\b))) input)
-              (values output index))))
+  (is (epmv (split-sequence-if-not (lambda (x) (member x '(#\a #\b))) input)
+            (values output index))))
+
+(define-test split-sequence-if-not.2 (:input (input "12121212121")
+                                      :output (output ("12121" "1" "1" "1"))
+                                      :index (index 0))
+  (is (epmv (let ((count 3))
+              (split-sequence-if-not (lambda (c) (not (and (eql c #\2) (>= (decf count) 0))))
+                                     input
+                                     :from-end t))
+            (values output index))))
 
 (test split-sequence.start-end-error
   (signals error (split-sequence 0 #(0 1 2 3) :start nil))
@@ -206,8 +277,12 @@
                               :count 0 :remove-empty-subseqs t))
              '(nil 2))))
 
+(test split-sequence.check-bounds
+  (signals error (split-sequence 2 '(1 2 3 4) :end 5)))
+
 ;;; FUZZ TEST
 
+#+(or)
 (test split-sequence.fuzz
   (fuzz :verbose nil :fiveamp t))
 
